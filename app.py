0b89febef7f82e1b29282d790d4ec41578f935a1
unknown <you@example.com>
2018-09-05 19:29:56 -0400

    postgresql code  added

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
+import os
+
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
 from security import authenticate,identity
+
 from resources.user import UserRegister
 from resources.item import Item,ItemList
 from resources.store import Store,StoreList
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_NOTIFICATIONS'] = False
 app.secret_key = "anin"
+
 api = Api(app)
 jwt = JWT(app, authenticate, identity)
 
